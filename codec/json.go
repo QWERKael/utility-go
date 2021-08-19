b980afdc91a0d2cdd1e00d9c86b5699d2b5a96a9
QWERKael <d9311@126.com>
2021-08-19 22:37:40 +0800

    增加了一些文件操作的函数

--- codec/json.go
+++ codec/json.go
@@ -1,7 +1,9 @@
 package codec
 
 import (
+	path2 "github.com/QWERKael/utility-go/path"
 	jsonIter "github.com/json-iterator/go"
+	"io/ioutil"
 )
 
 func EncodeJson(v interface{}) ([]byte, error) {
@@ -14,4 +16,17 @@ func DecodeJson(b []byte, v interface{}) error {
 	//var json = jsonIter.ConfigCompatibleWithStandardLibrary
 	//return jsonIter.Marshal(&v)
 	return jsonIter.Unmarshal(b, v)
+}
+
+func DecodeJsonFromFile(path string, v interface{}) error {
+	file, err := path2.OpenFileIfExist(path)
+	if err != nil {
+		return err
+	}
+	var b []byte
+	b, err = ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	return DecodeJson(b, v)
 }
--- path/file.go
+++ path/file.go
@@ -17,6 +17,13 @@ const (
 	Dir      FileType = 3
 )
 
+//
+// CheckPath
+//  @Description: 检查文件的路径
+//  @param path 传入路径
+//  @return FileType 返回文件的类型
+//  @return error 当路径为空，或者获取路径信息失败时，报错
+//
 func CheckPath(path string) (FileType, error) {
 	if path == "" {
 		return Unknown, errors.New("path is nil")
@@ -36,6 +43,24 @@ func CheckPath(path string) (FileType, error) {
 	}
 }
 
+func CheckPathAs(path string, checkFt FileType) (bool, error) {
+	if ft, err := CheckPath(path); err != nil {
+		return false, err
+	} else {
+		if ft == checkFt {
+			return true, nil
+		} else {
+			return false, nil
+		}
+	}
+}
+
+//
+// CheckAndCreateDir
+//  @Description: 检查并创建目录
+//  @param path 传入路径
+//  @return error 无法识别path或者path为文件时，报错
+//
 func CheckAndCreateDir(path string) error {
 	ft, err := CheckPath(path)
 	if err != nil {
@@ -53,8 +78,60 @@ func CheckAndCreateDir(path string) error {
 	return errors.New("确认目录时遇到了一个未知的错误")
 }
 
+//
+// OpenFileIfExist
+//  @Description: 如果文件存在，则打开，否则报错
+//  @param path
+//  @return *os.File
+//  @return error
+//
+func OpenFileIfExist(path string) (*os.File, error) {
+	if ok, err := CheckPathAs(path, File); err != nil {
+		return nil, err
+	} else if ok {
+		file, err := os.Open(path)
+		if err != nil {
+			return nil, err
+		}
+		return file, nil
+	} else {
+		return nil, errors.New("传入路径不是文件类型")
+	}
+}
+
+//
+// CreateFileIfNotExist
+//  @Description: 如果文件不存在，则创建，否则报错
+//  @param path
+//  @return *os.File
+//  @return error
+//
+func CreateFileIfNotExist(path string) (*os.File, error) {
+	if ok, err := CheckPathAs(path, NotExist); err != nil {
+		return nil, err
+	} else if ok {
+		file, err := os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+		return file, nil
+	} else {
+		return nil, errors.New("传入路径已存在")
+	}
+}
+
+//
+// CheckAndOpenFile
+//  @Description: 打开一个文件，如果文件存在就直接打开，如果不存在就创建然后打开
+//  @param path
+//  @return *os.File
+//  @return error
+//
 func CheckAndOpenFile(path string) (*os.File, error) {
 	ft, err := CheckPath(path)
+	if err != nil {
+		return nil, err
+	}
 	var file *os.File
 	switch ft {
 	case NotExist:
@@ -70,8 +147,15 @@ func CheckAndOpenFile(path string) (*os.File, error) {
 	return file, nil
 }
 
+//
+// SumMd5FromFile
+//  @Description: 校验文件的md5值，返回字符串
+//  @param fileName
+//  @return string
+//  @return error
+//
 func SumMd5FromFile(fileName string) (string, error) {
-	file, err := os.Open(fileName)
+	file, err := OpenFileIfExist(fileName)
 	if err != nil {
 		return "", err
 	}
@@ -83,5 +167,3 @@ func SumMd5FromFile(fileName string) (string, error) {
 	Md5Str := fmt.Sprintf("%x", m.Sum(nil))
 	return Md5Str, nil
 }
-
-
