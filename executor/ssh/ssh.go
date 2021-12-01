28d95d73146581244185639f0176e935cbcbb8b0
QWERKael <d9311@126.com>
2021-12-01 18:17:06 +0800

    新增通过sftp读写远程文件的功能

--- executor/ssh/ssh.go
+++ executor/ssh/ssh.go
@@ -1,8 +1,12 @@
 package ssh
 
 import (
+	"errors"
 	"fmt"
+	"github.com/QWERKael/utility-go/path"
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
+	"os"
 	"time"
 )
 
@@ -14,6 +18,7 @@ type SSH struct {
 	Config     *ssh.ClientConfig
 	Client     *ssh.Client
 	Session    *ssh.Session
+	SFTPClient *sftp.Client
 }
 
 func getAuthMethodByPrivateKey(privateKey []byte) (ssh.AuthMethod, error) {
@@ -33,6 +38,7 @@ func NewSSHByPrivateKey(host string, user string, port int, privateKey []byte) (
 		Config:     nil,
 		Client:     nil,
 		Session:    nil,
+		SFTPClient: nil,
 	}
 	am, err := getAuthMethodByPrivateKey(privateKey)
 	if err != nil {
@@ -55,25 +61,79 @@ func NewSSHByPrivateKey(host string, user string, port int, privateKey []byte) (
 	if err != nil {
 		return nil, err
 	}
+	s.SFTPClient, err = sftp.NewClient(s.Client)
+	if err != nil {
+		return nil, err
+	}
 	return s, nil
 }
 
 func (s *SSH) Destruct() {
-	s.Session.Close()
-	s.Client.Close()
+	_ = s.Session.Close()
+	_ = s.SFTPClient.Close()
+	_ = s.Client.Close()
 }
 
-func (s *SSH) SetMultipleCommand(commands []string) error {
-	commands = append(commands, "exit")
-	stdin, err := s.Session.StdinPipe()
+//func (s *SSH) SetMultipleCommand(commands []string) error {
+//	commands = append(commands, "exit")
+//	stdin, err := s.Session.StdinPipe()
+//	if err != nil {
+//		return err
+//	}
+//	for _, cmd := range commands {
+//		_, err = fmt.Fprintf(stdin, "%s\n", cmd)
+//		if err != nil {
+//			return err
+//		}
+//	}
+//	return nil
+//}
+
+func (s *SSH) CheckRemotePath(remotePath string) (path.FileType, error) {
+	if remotePath == "" {
+		return path.Unknown, errors.New("path is nil")
+	}
+	fi, err := s.SFTPClient.Lstat(remotePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return path.NotExist, nil
+		} else {
+			return path.Unknown, err
+		}
+	}
+	if fi.IsDir() {
+		return path.Dir, nil
+	} else {
+		return path.File, nil
+	}
+}
+
+func (s *SSH) WriteRemoteFile(remotePath string, data []byte) error {
+	if ft, _ := s.CheckRemotePath(remotePath); ft != path.NotExist {
+		return errors.New("指定的文件路径不为空")
+	}
+	f, err := s.SFTPClient.Create(remotePath)
 	if err != nil {
 		return err
 	}
-	for _, cmd := range commands {
-		_, err = fmt.Fprintf(stdin, "%s\n", cmd)
-		if err != nil {
-			return err
-		}
+	_, err = f.Write(data)
+	if err != nil {
+		return err
 	}
+	_ = f.Close()
 	return nil
+}
+
+func (s *SSH) ReadRemoteFile(remotePath string) ([]byte, error) {
+	f, err := s.SFTPClient.Open(remotePath)
+	if err != nil {
+		return nil, err
+	}
+	var data []byte
+	_, err = f.Read(data)
+	if err != nil {
+		return nil, err
+	}
+	_ = f.Close()
+	return data, nil
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,8 @@ require (
 	github.com/kataras/tablewriter v0.0.0-20180708051242-e063d29b7c23 // indirect
 	github.com/lensesio/tableprinter v0.0.0-20201125135848-89e81fc956e7
 	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/pkg/sftp v1.13.4
 	go.uber.org/zap v1.16.0
-	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 	gopkg.in/yaml.v2 v2.4.0
 )
