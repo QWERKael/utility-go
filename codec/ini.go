697adf63f45ca660c22cec72fc1e18e0ad1b33ae
QWERKael <d9311@126.com>
2021-12-29 17:06:08 +0800

    ini文件解析，增加DecodeIniAllSection功能，解析全部section

--- codec/ini.go
+++ codec/ini.go
@@ -5,6 +5,25 @@ import (
 	"gopkg.in/ini.v1"
 )
 
+func DecodeIniAllSection(path string) (map[string]map[string]string, error) {
+	f, err := ini.LoadSources(ini.LoadOptions{
+		AllowBooleanKeys: true,
+	}, path)
+	if err != nil {
+		return nil, err
+	}
+
+	f.Sections()
+
+	m := make(map[string]map[string]string)
+
+	sections := f.Sections()
+	for _, section := range sections {
+		m[section.Name()] = section.KeysHash()
+	}
+	return m, nil
+}
+
 func DecodeIniSection(b []byte, sectionName string) (map[string]string, error) {
 	f, err := ini.LoadSources(ini.LoadOptions{
 		AllowBooleanKeys: true,
