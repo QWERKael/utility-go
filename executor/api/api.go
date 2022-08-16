fd0dd1a2375ae577891d72401316d931c9d31ac1
QWERKael <d9311@126.com>
2022-08-16 18:26:19 +0800

    优化api模块，支持多种常用的http请求

--- executor/api/api.go
+++ executor/api/api.go
@@ -6,39 +6,50 @@ import (
 	"strings"
 )
 
-func Get(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	var body []byte
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
+// 表示请求未成功的状态码
+var errStatusCode = 0
+
+func Get(url string, headers map[string]string, urlQuery map[string]string) (int, []byte, error) {
+	return Request(http.MethodGet, url, headers, urlQuery, nil)
 }
 
-func Put(url string, data []byte) ([]byte, error) {
+func Post(url string, headers map[string]string, urlQuery map[string]string, data []byte) (int, []byte, error) {
+	return Request(http.MethodPost, url, headers, urlQuery, data)
+}
+
+func Put(url string, headers map[string]string, urlQuery map[string]string, data []byte) (int, []byte, error) {
+	return Request(http.MethodPut, url, headers, urlQuery, data)
+}
+
+func Request(method string, url string, headers map[string]string, urlQuery map[string]string, data []byte) (int, []byte, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("PUT", url, strings.NewReader(string(data)))
+	// 创建新的 request
+	req, err := http.NewRequest(method, url, strings.NewReader(string(data)))
 	if err != nil {
-		return nil, err
+		return errStatusCode, nil, err
 	}
+	// 添加 header
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	// 添加 url query
+	q := req.URL.Query()
+	for queryKey, queryValue := range urlQuery {
+		q.Set(queryKey, queryValue)
+	}
+	req.URL.RawQuery = q.Encode()
+	// 执行 request
 	var resp *http.Response
 	resp, err = client.Do(req)
 	if err != nil {
-		return nil, err
+		return errStatusCode, nil, err
 	}
 	defer resp.Body.Close()
+	// 返回结果
 	var body []byte
 	body, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return errStatusCode, nil, err
 	}
-	return body, nil
+	return resp.StatusCode, body, nil
 }
