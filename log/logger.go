fce02dcd08f3a4a6dc84dfdaa77f3d27867e7754
QWERKael <d9311@126.com>
2023-02-20 15:48:51 +0800

    修复了日志切分功能

--- log/logger.go
+++ log/logger.go
@@ -1,13 +1,11 @@
 package log
 
 import (
-	"fmt"
-	"github.com/QWERKael/utility-go/path"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"os"
 	"strings"
-	"time"
 )
 
 var SugarLogger *zap.SugaredLogger
@@ -20,11 +18,11 @@ const (
 )
 
 func InitLogger(logPath, LogLevel string) *zap.SugaredLogger {
-	now := time.Now().Format("2006_01_02_15")
-	if logPath != "" {
-		logPath = fmt.Sprintf("%s.%s", logPath, now)
-	}
-	sugarLogger, err := NewLogger(ConsoleEncoder, logPath, ConvertLogLevel(LogLevel))
+	//now := time.Now().Format("2006_01_02_15")
+	//if logPath != "" {
+	//	logPath = fmt.Sprintf("%s.%s", logPath, now)
+	//}
+	sugarLogger, err := NewLogger(ConsoleEncoder, logPath, ConvertLogLevel(LogLevel), 128, 30, 60)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -33,7 +31,7 @@ func InitLogger(logPath, LogLevel string) *zap.SugaredLogger {
 	return sugarLogger
 }
 
-func NewLogger(encoderFormat EncoderFormat, logFilePath string, level zapcore.Level) (*zap.SugaredLogger, error) {
+func NewLogger(encoderFormat EncoderFormat, logFilePath string, level zapcore.Level, maxSize, maxAge, maxBackups int) (*zap.SugaredLogger, error) {
 	var encoder zapcore.Encoder
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
@@ -49,11 +47,18 @@ func NewLogger(encoderFormat EncoderFormat, logFilePath string, level zapcore.Le
 	if logFilePath == "" {
 		writeSyncer = zapcore.Lock(os.Stdout)
 	} else {
-		file, err := path.CreateOrOpenFileForAppendWrite(logFilePath)
-		if err != nil {
-			return nil, err
+		//file, err := path.CreateOrOpenFileForAppendWrite(logFilePath)
+		//if err != nil {
+		//	return nil, err
+		//}
+		lumberJackLogger := &lumberjack.Logger{
+			Filename:   logFilePath, // 文件位置
+			MaxSize:    maxSize,     // 进行切割之前,日志文件的最大大小(MB为单位)
+			MaxAge:     maxAge,      // 保留旧文件的最大天数
+			MaxBackups: maxBackups,  // 保留旧文件的最大个数
+			Compress:   false,       // 是否压缩/归档旧文件
 		}
-		writeSyncer = zapcore.AddSync(file)
+		writeSyncer = zapcore.AddSync(lumberJackLogger)
 	}
 
 	core := zapcore.NewCore(encoder, writeSyncer, level)
