fcf168b4344defb8621a852fc99541295eacb598
QWERKael <d9311@126.com>
2024-11-14 17:48:06 +0800

    使用go-mysql/client连接mysql数据库

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -4,45 +4,25 @@ import (
 	"fmt"
 	"github.com/QWERKael/utility-go/log"
 	"github.com/go-mysql-org/go-mysql/client"
-	_ "github.com/go-sql-driver/mysql"
 )
-
-type ConnectInfo struct {
-	userName string
-	password string
-	network  string
-	host     string
-	port     int
-	database string
-}
 
 type Connector struct {
 	DB *client.Conn
 }
 
-func (ci *ConnectInfo) NewConnector() (*Connector, error) {
-	conn := &Connector{}
-	err := conn.Connect(ci.userName, ci.password, ci.host, ci.port, ci.database)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}
-
-func (c *Connector) Connect(userName string, password string, host string, port int, database string) error {
+func Connect(userName string, password string, host string, port int, database string) (Connector, error) {
 	conn, err := client.Connect(fmt.Sprintf("%s:%d", host, port), userName, password, database, nil)
 	if err != nil {
 		log.SugarLogger.Debugf("连接到【%s:%d】失败", host, port)
-		return err
+		return Connector{}, err
 	}
-	c.DB = conn
-	return nil
+	return Connector{DB: conn}, nil
 }
 
-func (c *Connector) QueryAsMapList(sql string) ([]map[string]interface{}, error) {
+func (c *Connector) QueryAsMapListWithColNames(sql string) ([]map[string]interface{}, []string, error) {
 	r, err := c.DB.Execute(sql)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer r.Close()
 	colNames := make([]string, r.ColumnNumber())
@@ -56,7 +36,12 @@ func (c *Connector) QueryAsMapList(sql string) ([]map[string]interface{}, error)
 			resultSet[i][colNames[j]] = col.Value()
 		}
 	}
-	return resultSet, nil
+	return resultSet, colNames, nil
+}
+
+func (c *Connector) QueryAsMapList(sql string) ([]map[string]interface{}, error) {
+	resultSet, _, err := c.QueryAsMapListWithColNames(sql)
+	return resultSet, err
 }
 
 func (c *Connector) QueryAsMapStringListWithColNames(sql string) ([]map[string]string, []string, error) {
