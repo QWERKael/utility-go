442892e111b92a3203b783c1aff65f26d2bc5e0a
QWERKael <d9311@126.com>
2024-11-14 15:42:41 +0800

    使用go-mysql/client连接mysql数据库

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -1,9 +1,9 @@
 package mysql
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/QWERKael/utility-go/log"
+	"github.com/go-mysql-org/go-mysql/client"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -17,96 +17,64 @@ type ConnectInfo struct {
 }
 
 type Connector struct {
-	DB *sql.DB
+	DB *client.Conn
 }
 
-func NewConnector(ci *ConnectInfo) (*Connector, error) {
+func (ci *ConnectInfo) NewConnector() (*Connector, error) {
 	conn := &Connector{}
-	err := conn.Connect(ci.userName, ci.password, ci.network, ci.host, ci.port, ci.database)
+	err := conn.Connect(ci.userName, ci.password, ci.host, ci.port, ci.database)
 	if err != nil {
 		return nil, err
 	}
 	return conn, nil
 }
 
-func (c *Connector) Connect(userName string, password string, network string, host string, port int, database string) error {
-	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s", userName, password, network, host, port, database)
-	return c.ConnectWithDSN(dsn)
-}
-
-func (c *Connector) ConnectWithDSN(dsn string) error {
-	log.SugarLogger.Debugf("连接到【%s】", dsn)
-	var err error
-	c.DB, err = sql.Open("mysql", dsn)
-	return err
+func (c *Connector) Connect(userName string, password string, host string, port int, database string) error {
+	conn, err := client.Connect(fmt.Sprintf("%s:%d", host, port), userName, password, database, nil)
+	if err != nil {
+		log.SugarLogger.Debugf("连接到【%s:%d】失败", host, port)
+		return err
+	}
+	c.DB = conn
+	return nil
 }
 
 func (c *Connector) QueryAsMapList(sql string) ([]map[string]interface{}, error) {
-	rows, err := c.DB.Query(sql)
+	r, err := c.DB.Execute(sql)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
-	var colNames []string
-	colNames, err = rows.Columns()
-	if err != nil {
-		return nil, err
+	defer r.Close()
+	colNames := make([]string, r.ColumnNumber())
+	for colName, i := range r.FieldNames {
+		colNames[i] = colName
 	}
-	var resultSet = make([]map[string]interface{}, 0)
-
-	for rows.Next() {
-		cols := make([]interface{}, len(colNames))
-		colPtrs := make([]interface{}, len(colNames))
-		for i, _ := range cols {
-			colPtrs[i] = &cols[i]
+	var resultSet = make([]map[string]interface{}, r.RowNumber())
+	for i, row := range r.Values {
+		resultSet[i] = make(map[string]interface{})
+		for j, col := range row {
+			resultSet[i][colNames[j]] = col.Value()
 		}
-		if err := rows.Scan(colPtrs...); err != nil {
-			return nil, err
-		}
-		result := make(map[string]interface{})
-		for i, colName := range colNames {
-			v := colPtrs[i].(*interface{})
-			result[colName] = *v
-		}
-		resultSet = append(resultSet, result)
 	}
 	return resultSet, nil
 }
 
 func (c *Connector) QueryAsMapStringListWithColNames(sql string) ([]map[string]string, []string, error) {
-	rows, err := c.DB.Query(sql)
+	r, err := c.DB.Execute(sql)
 	if err != nil {
 		return nil, nil, err
 	}
-	defer rows.Close()
-	var colNames []string
-	colNames, err = rows.Columns()
-	if err != nil {
-		return nil, nil, err
+	defer r.Close()
+	colNames := make([]string, r.ColumnNumber())
+	for colName, i := range r.FieldNames {
+		colNames[i] = colName
 	}
-	var resultSet = make([]map[string]string, 0)
-
-	for rows.Next() {
-		cols := make([]interface{}, len(colNames))
-		colPtrs := make([]interface{}, len(colNames))
-		for i, _ := range cols {
-			colPtrs[i] = &cols[i]
+	var resultSet = make([]map[string]string, r.RowNumber())
+	for i, row := range r.Values {
+		resultSet[i] = make(map[string]string)
+		for j, col := range row {
+			resultSet[i][colNames[j]] = col.String()
 		}
-		if err := rows.Scan(colPtrs...); err != nil {
-			return nil, nil, err
-		}
-		result := make(map[string]string)
-		for i, colName := range colNames {
-			v := colPtrs[i].(*interface{})
-			switch {
-			case *v == nil:
-				result[colName] = "NULL"
-				continue
-			default:
-				result[colName] = fmt.Sprintf("%s", *v)
-			}
-		}
-		resultSet = append(resultSet, result)
 	}
 	return resultSet, colNames, nil
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/QWERKael/utility-go
 go 1.19
 
 require (
-	github.com/BurntSushi/toml v0.3.1
+	github.com/BurntSushi/toml v1.3.2
+	github.com/go-mysql-org/go-mysql v1.9.1
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/json-iterator/go v1.1.10
 	github.com/lensesio/tableprinter v0.0.0-20201125135848-89e81fc956e7
 	github.com/pkg/sftp v1.13.4
-	go.uber.org/zap v1.16.0
+	go.uber.org/zap v1.26.0
 	golang.org/x/crypto v0.11.0
 	gopkg.in/ini.v1 v1.66.2
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
@@ -18,16 +19,25 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/kataras/tablewriter v0.0.0-20180708051242-e063d29b7c23 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pingcap/errors v0.11.5-0.20221009092201-b66cddb77c32 // indirect
+	github.com/pingcap/log v1.1.1-0.20230317032135-a0d097d16e22 // indirect
+	github.com/pingcap/tidb/pkg/parser v0.0.0-20231103042308-035ad5ccbe67 // indirect
 	github.com/rivo/uniseg v0.1.0 // indirect
-	go.uber.org/atomic v1.6.0 // indirect
-	go.uber.org/multierr v1.5.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
+	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
